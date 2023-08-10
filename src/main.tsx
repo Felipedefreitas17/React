9b08f2628c024901a55cba7f57079fecc52f2c8e
Felipe <felipedefreitas177@gmail.com>
2023-08-09 21:52:56 -0300

    Inclusao Header

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import ReactDOM from 'react-dom/client'
 //componentes
 import Home from './pages/Home/';
 import ListaServicos from './pages/ListaServicos';
+import Footer from './componentes/Footer';
+import Header from './componentes/Header';
 
 //Estilezacao global
 import"./index.css"
 
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+
+    <Header />
     <Home />
+    <Footer />
+    
      {/* <ListaServicos />  */}
   </React.StrictMode>,
 )
